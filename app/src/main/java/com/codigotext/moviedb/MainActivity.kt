0b97404eb24f66091e1e1cc1a4a6330d9f14d21f
7th Computing Developer <devuser@7thcomputing.com>
2021-10-07 14:34:30 +0630

    hanlde lifecycle

--- app/src/main/java/com/codigotext/moviedb/MainActivity.kt
+++ app/src/main/java/com/codigotext/moviedb/MainActivity.kt
@@ -2,6 +2,7 @@ package com.codigotext.moviedb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.codigotext.moviedb.databinding.ActivityMainBinding
 import com.codigotext.moviedb.ui.populator_movie.FragmentPopulatorMovie
 import com.codigotext.moviedb.ui.upcoming_movie.FragmentUpcomeMovies
@@ -14,25 +15,32 @@ private const val POPULATOR_MOVIE_FRA_TAG = "populator"
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    private val fragmentUpcomeMovies: FragmentUpcomeMovies = FragmentUpcomeMovies.newInstance()
+    private val fragmentPopulatorMovie: FragmentPopulatorMovie =
+        FragmentPopulatorMovie.newInstance()
+    private var activeFragment: Fragment = fragmentUpcomeMovies
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        selectDefaultFragment()
+        initTabFragment()
 
         findViewById<BottomNavigationView>(R.id.bottom_nav).setOnNavigationItemSelectedListener {
             when (it.itemId) {
                 R.id.nav_poppulator -> {
-                    supportFragmentManager.beginTransaction().replace(
-                        R.id.fragment_container, FragmentPopulatorMovie.newInstance(),
-                        POPULATOR_MOVIE_FRA_TAG
-                    ).commit()
+                    supportFragmentManager.beginTransaction().hide(activeFragment)
+                        .show(fragmentPopulatorMovie)
+                        .commit()
+                    activeFragment = fragmentPopulatorMovie
+
                 }
                 R.id.nav_home -> {
-                    supportFragmentManager.beginTransaction().replace(
-                        R.id.fragment_container, FragmentUpcomeMovies.newInstance(),
-                        UPCOMING_MOVIE_FRA_TAG
-                    ).commit()
+                    supportFragmentManager.beginTransaction().hide(activeFragment)
+                        .show(fragmentUpcomeMovies)
+                        .commit()
+                    activeFragment = fragmentUpcomeMovies
+
                 }
             }
             true
@@ -58,13 +66,21 @@ class MainActivity : AppCompatActivity() {
          }*/
     }
 
-    private fun selectDefaultFragment() {
+    private fun initTabFragment() {
         supportFragmentManager.beginTransaction()
-            .replace(
+            .add(
                 R.id.fragment_container,
-                FragmentUpcomeMovies.newInstance(),
+                fragmentUpcomeMovies,
                 UPCOMING_MOVIE_FRA_TAG
             )
+            .commit()
+        supportFragmentManager.beginTransaction()
+            .add(
+                R.id.fragment_container,
+                fragmentPopulatorMovie,
+                POPULATOR_MOVIE_FRA_TAG
+            )
+            .hide(fragmentPopulatorMovie)
             .commit()
     }
 
